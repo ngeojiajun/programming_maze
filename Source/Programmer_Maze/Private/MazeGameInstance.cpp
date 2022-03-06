fcd434466274dbbc628b3cb8893c9eca0b1ab9e7
Junichi Fujiwara <fujiwarajunichi2000@outlook.jp>
2022-03-06 16:51:49 +0900

    setup the sound volume management

--- Source/Programmer_Maze/Private/MazeGameInstance.cpp
+++ Source/Programmer_Maze/Private/MazeGameInstance.cpp
@@ -2,4 +2,64 @@
 
 
 #include "MazeGameInstance.h"
-//THIS FILE IS INTENTIONALLY LEFT EMPTY
+#include "UObject/ConstructorHelpers.h"
+#include "Kismet/GameplayStatics.h"
+#include "GeneralUtilities.h"
+
+UMazeGameInstance::UMazeGameInstance(const FObjectInitializer& ObjectInitializer):UGameInstance(ObjectInitializer) {
+	static ConstructorHelpers::FObjectFinder<USoundMix> SC_Asset(TEXT("/Game/Sounds/Classes/SCMix_Master.SCMix_Master"));
+	static ConstructorHelpers::FObjectFinder<USoundClass> SCMaster(TEXT("/Game/Sounds/Classes/SC_Master.SC_Master"));
+	static ConstructorHelpers::FObjectFinder<USoundClass> SCBGM(TEXT("/Game/Sounds/Classes/SC_BGM.SC_BGM"));
+	static ConstructorHelpers::FObjectFinder<USoundClass> SCSFX(TEXT("/Game/Sounds/Classes/SC_SFX.SC_SFX"));
+	checkpointID = 0;
+	if (SC_Asset.Succeeded()) {
+		mixer = SC_Asset.Object;
+	}
+	if (SCMaster.Succeeded()) {
+		masterClass = SCMaster.Object;
+	}
+	if (SCBGM.Succeeded()) {
+		BGMClass = SCBGM.Object;
+	}
+	if (SCSFX.Succeeded()) {
+		SFXClass = SCSFX.Object;
+	}
+}
+
+void UMazeGameInstance::Init() {
+	if (!mixer || !masterClass || !BGMClass || !SFXClass) {
+		GeneralUtilities::Log(this, TEXT("Audio engine initialization failed... Volume control will not work"));
+		GeneralUtilities::LogBoolean(this, !mixer, TEXT("!mixer"));
+		GeneralUtilities::LogBoolean(this, !masterClass, TEXT("!masterClass"));
+		GeneralUtilities::LogBoolean(this, !BGMClass, TEXT("!BGMClass"));
+		GeneralUtilities::LogBoolean(this, !SFXClass, TEXT("!SFXClass"));
+	}
+	else {
+		UGameplayStatics::SetBaseSoundMix(this, mixer);
+		initializationDone = true;
+	}
+}
+
+void UMazeGameInstance::setVolume(SoundType type, float volume)
+{
+	if (!initializationDone) {
+		return;
+	}
+	USoundClass* clzz = NULL;
+	switch (type) {
+	case SoundType::BGM:
+		clzz = BGMClass;
+		break;
+	case SoundType::Master:
+		clzz = masterClass;
+		break;
+	case SoundType::SFX:
+		clzz = SFXClass;
+		break;
+	default:
+		checkNoEntry();
+	}
+	//set the override with the original values except the volume to the requested and the fade in time to zero to enforce it immediately
+	UGameplayStatics::SetSoundMixClassOverride(this, mixer, clzz, volume, 1.0f, 0.0f, true);
+	//TODO save this setting
+}
--- Source/Programmer_Maze/Public/MazeGameInstance.h
+++ Source/Programmer_Maze/Public/MazeGameInstance.h
@@ -4,7 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
+#include "Sound/SoundMix.h"
+#include "Sound/SoundClass.h"
 #include "MazeGameInstance.generated.h"
+
+enum class SoundType {
+	BGM,SFX,Master
+};
 
 /**
  * The game instances which this game will use
@@ -14,7 +20,27 @@ class PROGRAMMER_MAZE_API UMazeGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
 public:
+	UMazeGameInstance(const FObjectInitializer& ObjectInitializer);
+	//initialize the instance
+	virtual void Init();
+	//adjust the volume
+	void setVolume(SoundType type, float volume);
 	//checkpoint id that the map shall teleports to
 	UPROPERTY()
 	int checkpointID;
+private:
+	//the global sound mixer for the volume control
+	UPROPERTY()
+	USoundMix* mixer;
+	//sound class for identifing the sound type
+	UPROPERTY()
+	USoundClass* masterClass; //to be removed as we never use it in production, just for the PoC
+	//BGM sound class
+	UPROPERTY()
+	USoundClass* BGMClass;
+	//SFX sound class
+	UPROPERTY()
+	USoundClass* SFXClass;
+	//weather the resolution succeeded
+	bool initializationDone;
 };
